8db3924876510ef779233d8122a4dbb52c04733d
luzatti-alt <lucasguzatti4@gmail.com>
2026-02-13 11:53:01 -0300

    base api e outras dependencias

--- api.py
+++ api.py
@@ -1,1 +1,15 @@
 #comm websocket/bot etc
+#api rest
+from flask import Flask,request,jsonify
+from flask_bcrypt import Bcrypt  # criptografia
+
+api = Flask(__name__)
+bcrypt = Bcrypt(api)
+
+@api.route("/", methods=["GET"])
+def raiz():
+    return jsonify({"status": "ok", "mensagem": "API AHN funcionando!"})
+
+#rodar api
+if __name__ == "__main__":
+    api.run(host='0.0.0.0')
